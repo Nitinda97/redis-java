410103deb5f57c0f3ecd974fe423aa05dd9e0d89
nitinda97 <nitinda97@gmail.com>
2025-10-31 17:42:37 +0530

    added ping-pong functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,6 +21,12 @@ public class Main {
                   serverSocket.setReuseAddress(true);
     //      // Wait for connection from client.
                   clientSocket = serverSocket.accept();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                String message = in.readLine();
+                System.out.println("Received from client: " + message);
+                out.println("+PONG\\r\\n");
                 }
             catch (IOException e) {
                     System.out.println("IOException: " + e.getMessage());
